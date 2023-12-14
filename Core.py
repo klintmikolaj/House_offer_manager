145a9c06df3038b0f68e4af684220f94ccb92fe0
klintmikolaj <klintmikolaj@gmail.com>
2023-12-14 12:05:24 +0100

    Some db functions

--- Core.py
+++ Core.py
@@ -9,8 +9,8 @@ class Core:
     def __init__(self):
         self.domain = "https://www.otodom.pl"
         self.offer_list = [] #lista przechowuje obiekty dataclass
-        self.data_base = ... #obiekt MySQL.connector, przechowuje połączenie do bazy danych, zatwierdzamy zmiany w bazie danych
-        self.data_base_cursor = ... #wysyłanie query do bazy danych
+        self.db = mysql.connector.connect(host="localhost",  user="root", passwd="eloelo320") #obiekt MySQL.connector, przechowuje połączenie do bazy danych, zatwierdzamy zmiany w bazie danych
+        self.db_cursor = self.db.cursor()
         self.headers = OrderedDict()
         self.page_id = ""
 
@@ -32,6 +32,7 @@ class Core:
 
     def get_page_and_scrape_data(self, offer_url):
         try:
+            offer_id = offer_url
             with requests.session() as session:
                 session.headers = self.headers
                 response = session.get(offer_url, timeout=25)
@@ -42,8 +43,11 @@ class Core:
             price_per_m2 = re.search(r'(?<="key":"price_per_m","value":")(\d+)', response.text).group(1)
             district = re.search(r'(district":{"id":"\d*","code":")(.+?)(?=")', response.text).group(2)
             add_date = re.search(r'(?<=,"createdAt":")(.+?)(?=")', response.text).group(1)
+            print(offer_id)
+            print(price)
+            print (street)
 
-            self.offer_list.append(Offer(price, street, room_count, price_per_m2, district, add_date))
+            self.offer_list.append(Offer(offer_id, price, street, room_count, price_per_m2, district, add_date))
 
         except Exception as exc:
             print(exc)
@@ -63,23 +67,43 @@ class Core:
         results = results[3:]
 
         for result in results:
-            self.get_page_and_scrape_data(self.domain + result, self.headers)
-            time.sleep(5)
+            self.get_page_and_scrape_data(self.domain + result)
+            time.sleep(1)
 
-    def upload_to_database(self):
-        ...
+    def create_database(self):
+        # Create a new database if it doesn't exist
+        self.db_cursor.execute("CREATE DATABASE IF NOT EXISTS homes")
+        self.db_cursor.execute("USE homes")
+        self.db_cursor.execute("""
+            CREATE TABLE IF NOT EXISTS offers (
+                offerID VARCHAR(200) PRIMARY KEY,
+                price INT UNSIGNED,
+                street VARCHAR(200),
+                room_count SMALLINT UNSIGNED,
+                price_per_m2 SMALLINT UNSIGNED,
+                district VARCHAR(50),
+                add_date DATETIME
+            )
+        """)
+        self.db.commit()
 
     def connect_database(self):
         ...
     ''' laczy sie z baza  '''
+    def upload_to_database(self):
+        for offer in self.offer_list:
+            self.db_cursor.execute("INSERT INTO offers (offer_id, price, street, room_count, price_per_m2, district, add_date) VALUES (%s, %s, %s, %s, %s, %s, %s)" % (
+            offer.offer_id, offer.price, offer.street, offer.room_count, offer.price_per_m2, offer.district, offer.add_date))
 
     def run(self):
-        ...
-    ''' wyciagaj liste ofert z zakladek, dla kazdej z ofert wywolaj getandscrapt, jak sie wypelni lista, connect to db, wywolaj metode upload to database'''
-
+        self.init_headers()
+        self.get_offer_list()
+        self.create_database()
+        self.upload_to_database()
 
 @dataclass(frozen=True) # Unchangeable dataclass
 class Offer:
+    offer_id: str
     price: int
     street: str
     room_count: int
@@ -88,8 +112,13 @@ class Offer:
     add_date: str
 
     def __str__(self):
-        return f"price: {self.price}, street: {self.street}, room_count: {self.room_count}, price_per_m2: {self.price_per_m2}, district: {self.district}, add_date: {self.add_date}"
+        return (f" offer_id: {self.offer_id}, price: {self.price}, street: {self.street}, room_count: {self.room_count}"
+                f", price_per_m2: {self.price_per_m2}, district: {self.district}, add_date: {self.add_date}")
 
     def to_db(self):
-        return "INSERT INTO offers (price, street, room_count, price_per_m2, district, add_date) VALUES (%s, %s, %s, %s, %s, %s)" % (
-        self.price, self.street, self.room_count, self.price_per_m2, self.district, self.add_date)
+        return ("INSERT INTO (offer_id, price, street, room_count, price_per_m2, district, add_date) VALUES (%s, %s, %s, %s, %s, %s, %s)" %
+                (self.offer_id, self.price, self.street, self.room_count, self.price_per_m2, self.district, self.add_date))
+
+p = Core()
+p.run()
+
--- Handler.py
+++ Handler.py
@@ -1,5 +1,22 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="eloelo320",
+    database="testdatabase"
+)
+
+mycursor = db.cursor()
+
+#mycursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+mycursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)", ("Andrzejek", 69))
+db.commit()
 
 
+mycursor.execute("SELECT * FROM Person")
+for x in mycursor:
+    print(x)
 
 
 
